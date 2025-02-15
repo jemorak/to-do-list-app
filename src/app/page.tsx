0e6b5fe8aae65cb4d23f99b9607ced8d3c32395b
jemorak <jemima.orakwue@hotmail.co.uk>
2025-02-15 20:21:12 +0000

    Added title animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { useState } from "react";
 import MenuContext from "@/components/MenuContext";
 import { useEffect } from "react";
+import {motion } from "framer-motion";
 
 export default function Page() {
     const [isChecked, setIsChecked] = useState(false);
@@ -21,7 +22,6 @@ export default function Page() {
         }
     }, []);
 
-  // Save tasks to localStorage whenever tasks change
     useEffect(() => {
         if (tasks.length > 0) {
         localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -57,14 +57,30 @@ export default function Page() {
         <div className="App">
         <div className="flex flex-col items-center justify-center p-10 m-10 ">
             <Image
-            src="/Blossom_Powerpuff_Girl.png"
-            alt="picture of blossom"
-            width={100}
-            height={100}
+                src="/Blossom_Powerpuff_Girl.png"
+                alt="picture of blossom"
+                width={100}
+                height={100}
             />
-            <div className="p-10 m-10 border-2 box-border border-pink-500 text-center bg-pink-200 rounded-md ">
-            <h1 className="text-3xl font-bold underline">To do list app</h1>
-            </div>
+            <motion.div initial="hidden" animate="visible" variants={{
+                hidden: {
+                    scale: .8,
+                    opacity: 0
+
+                },
+                visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                        delay: .2
+                    }
+                },
+            }}>
+                <div className="p-10 m-10 border-2 box-border border-pink-500 text-center bg-pink-200 rounded-md ">
+                    <h1 className="text-3xl font-bold underline">To do list app</h1>
+                </div>
+            </motion.div>
+            
 
             <div className="p-10 m-5 border-4 box-border border-pink-500 text-center w-1/2 bg-pink-200 rounded-md">
             <h2 className="text-2xl font-serif mb-4 font-bold">
